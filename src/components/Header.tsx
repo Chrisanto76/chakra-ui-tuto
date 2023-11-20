ca8acf1c954dc201696b106e7f5d9d15133b1b56
Chrisanto76 <antonypoure2@gmail.com>
2023-11-20 15:56:26 +0100

    Refactor pricing component and header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,22 @@ import { Box, Text, Heading } from '@chakra-ui/react';
 function Header() {
 	return (
 		<div>
-			<Box
-				as="section"
-				color="#F7FAFC"
-				bg={'#6B46C1'}
-				pt={'90px'}
-				pb={'198px'}
-				px={'32px'}
-			>
-				<Heading fontWeight="800" fontSize={'48px'}>
-					Simple pricing for your business
-				</Heading>
-				<Text fontSize={'24px'} fontWeight={'500'}>
-					Plans that are carefully crafted to suit your business.
-				</Text>
+			<Box pb={'112px'}>
+				<Box
+					as="section"
+					color="#F7FAFC"
+					bg={'#6B46C1'}
+					pt={'90px'}
+					pb={'198px'}
+					px={'32px'}
+				>
+					<Heading fontWeight="800" fontSize={'48px'}>
+						Simple pricing for your business
+					</Heading>
+					<Text fontSize={'24px'} fontWeight={'500'}>
+						Plans that are carefully crafted to suit your business.
+					</Text>
+				</Box>
 			</Box>
 		</div>
 	);
--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { Box, Flex, Text, Heading, Button } from '@chakra-ui/react';
+import {
+	Box,
+	Flex,
+	Text,
+	Heading,
+	Button,
+	HStack,
+	Icon,
+	StackProps,
+	Stack,
+} from '@chakra-ui/react';
+import { CheckIcon } from '../icons/Icon';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const ListItem = (_Props: StackProps) => {
+	const { children, ...rest } = _Props;
+	return (
+		<HStack as={'li'} spacing={'20px'} {...rest}>
+			<Icon as={CheckIcon} w={'22px'} h={'22px'} />
+			<Text>{children}</Text>
+		</HStack>
+	);
+};
 
 function Pricing() {
 	return (
-		<Box>
+		<Box
+			maxW={'994px'}
+			m={'auto'}
+			mt={'-256px'}
+			borderRadius={'12px'}
+			overflow={'hidden'}
+			boxShadow={
+				'0px 10px 10px -5px rgba(0, 0, 0, 0.04), 0px 20px 25px -5px rgba(0, 0, 0, 0.10)'
+			}
+		>
 			<Flex>
 				<Box bg={'#F0EAFB'} p={'60px'}>
 					<Text fontSize={'24px'} fontWeight={'800'}>
@@ -19,7 +49,18 @@ function Pricing() {
 						Get Started
 					</Button>
 				</Box>
-				<Box></Box>
+				<Box p={'60px'} fontSize={'18px'} bg={'white'}>
+					<Text textAlign={'left'}>
+						Access these features when you get this pricing package for your
+						business.
+					</Text>
+					<Stack as={'ul'} spacing={'20px'} pt={'24px'}>
+						<ListItem>International calling and messaging API</ListItem>
+						<ListItem>Additional phone numbers</ListItem>
+						<ListItem>Automated messages via Zapier</ListItem>
+						<ListItem>24/7 support and consulting</ListItem>
+					</Stack>
+				</Box>
 			</Flex>
 		</Box>
 	);
